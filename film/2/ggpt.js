5681c4f175e0a38d2e9aac1d0eeaee89f40200b5
axo36 <ilanrenon2@gmail.com>
2026-02-01 14:34:09 +0100

    Update ggpt.js

--- film/2/ggpt.js
+++ film/2/ggpt.js
@@ -77,7 +77,7 @@ function openPanel() {function openPanelFromButton(btn) {
     <p class="bieu">${hour}</p>
     <p class="nic">${formatImg} — ${film}</p>
     <p class="mere">${salle}<strong> • Durée : 1h17 - Fin prévue à</strong> ${end}</p>
-    <a href="download.html">
+    <a href="../../download.html">
         <button class="btn-reserver">Télécharger</button>
     </a>
     <button class="close-panel" onclick="closePanel()">Fermer</button>
